5f722f4837830d33cd4dead01bc079bfa55ce96b
blburns <blburns@pm.me>
2023-11-13 19:17:59 -0600

    created cleanup.sh and install_node_modules.sh files to cleanup pycache and node_modules directories and files

---
+++ app/static/assets/bootstrap/bootstrap.js
@@ -0,0 +1,1 @@
+https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.js
---
+++ bin/cleanup.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+
+function clean_pycache(){
+
+  files=$(find . -type f | grep -v venv | grep "__pycache__")
+  dirs=$(find . -type d | grep -v venv | grep "__pycache__")
+  node_modules_dir=$(find . -type d | grep -v venv | grep "node_modules")
+
+  echo ">>> Removing pycache files" ;
+  if [ -z "${files}" ]; then
+    echo "No files to remove"
+  else
+    for f in ${files[f]}; do
+      echo "Removing pycache file: ${f}" ;
+      ( rm "${f}" ; )
+    done
+  fi
+
+  echo ">>> Removing pycache dirs" ;
+  if [ -z "${dirs}" ]; then
+    echo "No dirs to remove"
+  else
+    for d in ${dirs[d]}; do
+      echo "Removing pycache dir: ${d}" ;
+      ( rmdir "${d}" ; )
+    done
+  fi
+}
+
+function remove_node_modules() {
+  echo ">>> Removing node_modules dirs" ;
+  if [ -z "${node_modules_dir}" ]; then
+    echo "No node_modules to remove"
+  else
+    ( rm -Rf node_modules ; )
+  fi
+}
+
+function cleanup_complete() {
+  echo "Cleanup Complete !!!"
+}
+
+function init(){
+  clean_pycache ;
+  remove_node_modules ;
+  cleanup_complete ;
+
+}
+
+init
