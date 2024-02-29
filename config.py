0c916c036edb3ec39284a67b1e24f223dd438f18
blburns <blburns@pm.me>
2024-02-28 18:53:41 -0600

    removed duplicate config.Config class ... because i forgot to before the previous commit after testing

--- config.py
+++ config.py
@@ -47,7 +47,6 @@ class Config:
     COMPRESSOR_DEBUG = environ.get("COMPRESSOR_DEBUG")
 
 
-
 class DbConfig:
     """Set the Database configuration variables from the .env file"""
     """NOTE: Only SQLite, MySQL, & PostgreSQL are working currently"""
@@ -213,40 +212,4 @@ class MailConfig:
     mail_ascii_attachments = environ.get('MAIL_ASCII_ATTACHMENTS')
 
 
-class Config:
-    """Set Flask configuration variables from .env file."""
 
-    # General Config
-    ENVIRONMENT = environ.get("ENVIRONMENT")
-
-    # Flask Config
-    FLASK_APP = environ.get('FLASK_APP')
-    FLASK_DEBUG = environ.get('FLASK_DEBUG')
-    SECRET_KEY = environ.get('SECRET_KEY')
-
-    # Flask-Session (redis)
-    # REDIS_URI = environ.get("REDIS_URI")
-    # SESSION_TYPE = "redis"
-    # SESSION_REDIS = redis.from_url(REDIS_URI)
-
-    # Flask-Session (filesystem)
-    SESSION_TYPE = environ.get('SESSION_TYPE')
-    SESSION_PERMANENT = environ.get('SESSION_PERMANENT')
-
-    # Flask-SQLAlchemy
-    Db_Config = DbConfig()
-    Users_Db_Config = UsersDbConfig()
-    Admins_Db_Config = AdminsDbConfig()
-    SQLALCHEMY_DATABASE_URI = Db_Config.uri
-    SQLALCHEMY_DATABASE_URI_ADMINS = Admins_Db_Config.uri
-    SQLALCHEMY_DATABASE_URI_USERS = Users_Db_Config.uri
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = False
-
-    # Flask-Assets (Optional)
-    LESS_BIN = system("which lessc")
-    ASSETS_DEBUG = False
-    LESS_RUN_IN_DEBUG = False
-    STATIC_FOLDER = "static"
-    TEMPLATES_FOLDER = "templates"
-    COMPRESSOR_DEBUG = environ.get("COMPRESSOR_DEBUG")
